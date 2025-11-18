001c8514df549f71ac426c0bb8d44704e5c95895
nicollealejandra26 <nicolle.carvajalg@uniagustiniana.edu.co>
2025-11-18 18:58:51 +0000

    yaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'love',
-  webDir: 'out'
+  webDir: 'public'
 };
 
 export default config;
